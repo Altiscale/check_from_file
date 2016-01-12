bc16dd31f90e0131b1d16a62d2441988504f0a6e
Travis Thompson <travis@altiscale.com>
2016-01-12 12:22:12 -0800

    Add support for age of output files

--- lib/check_from_file/check.rb
+++ lib/check_from_file/check.rb
@@ -5,17 +5,17 @@ module CheckFromFile
   class Check < Nagios::Plugin
     def initialize(options)
       @options = options
+      @warning = false
+      @critical = false
     end
 
     def critical?
-      unless @return == 0
-        @error = true
-        return false
-      end
+      @critical = true unless @return == 0
+      return @critical
     end
 
     def warning?
-      false # We don't support a warning state, just critical or ok
+      return @warning
     end
 
     def ok?
@@ -23,19 +23,33 @@ module CheckFromFile
     end
 
     def check
+      [:return, :stdout, :stderr].each do |file|
+        @age = (Time.now - File.stat(@options[file]).mtime).to_i
+        case
+        when @age >= @options[:file_age_critical]
+          @stale = @critical = true
+          break
+        when @age >= @options[:file_age_warning]
+          @stale = @warning = true
+          break
+        end
+      end
+
       @return = File.read(@options[:return]).to_i
       @stdout = File.read(@options[:stdout])
       @stderr = File.read(@options[:stderr])
     end
 
     def message
+      return "One or more output files are #{@age} seconds old!" if @stale
+
       ret = "Command: #{@options[:command]} returned "
-      if @error
+      if @critical
         ret << "#{@return}, STDOUT: #{@stdout}, STDERR: #{@stderr}"
       else
         ret << "successfully"
       end
-      ret
+      return ret
     end
   end
 end
